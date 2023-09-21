f138ca951e2a592d4c851a09543dfe522206b44e
Mike Donnalley <mdonnalley@salesforce.com>
2023-09-21 13:19:40 -0600

    feat: add flags and args

--- src/commands/cjs2.ts
+++ src/commands/cjs2.ts
@@ -1,7 +1,36 @@
-import {Command} from '@oclif/core'
+import {Args, Command, Flags, Interfaces} from '@oclif/core'
+
+type Result = {
+  args: Interfaces.InferredArgs<typeof CJS2.args>
+  flags: Interfaces.InferredFlags<typeof CJS2.flags>
+}
 
 export default class CJS2 extends Command {
-  async run(): Promise<void> {
+  static flags = {
+    optionalString: Flags.string(),
+    defaultString: Flags.string({
+      default: 'simple string default',
+    }),
+    defaultFnString: Flags.string({
+      default: async () => Promise.resolve('async fn default'),
+    }),
+  }
+
+  static args = {
+    optionalArg: Args.string(),
+    defaultArg: Args.string({
+      default: 'simple string default',
+    }),
+    defaultFnArg: Args.string({
+      default: async () => Promise.resolve('async fn default'),
+    }),
+  }
+
+  static enableJsonFlag = true
+
+  async run(): Promise<Result> {
+    const {args, flags} = await this.parse(CJS2)
     this.log(`hello I am a CJS plugin from ${this.config.root}!`)
+    return {args, flags}
   }
 }
